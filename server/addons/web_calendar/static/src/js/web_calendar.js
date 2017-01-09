d92d0415b4b4b9ec838af82dc503b43fb59c5455
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-01-09 20:00:03 +0000

    use moment for start/end

--- server/addons/web_calendar/static/src/js/web_calendar.js
+++ server/addons/web_calendar/static/src/js/web_calendar.js
@@ -249,7 +249,9 @@ var CalendarView = View.extend({
             .then(function (create_right) {
                 self.create_right = create_right;
                 self.ready.resolve();
-                setTimeout(function() {self.$el.fullCalendar('render');}, 100);
+                setTimeout(function() {
+                    self.$el.fullCalendar('render');
+                }, 100);
                 self.trigger('calendar_view_loaded', fv);
             });
         return $.when(edit_check, init);
